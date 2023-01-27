1555a4d63f1b777f4649e67604617447c66d78de
Dirk <dirkscgm@gmail.com>
2023-01-27 16:56:39 +0200

    Updating transform job and dependencies

--- scripts/lambdas/handle_step_function_status.py
+++ scripts/lambdas/handle_step_function_status.py
@@ -2,10 +2,12 @@ import json
 import os
 
 import boto3
+from botocore.exceptions import ClientError
 
 from codebase import get_logger
 
 # items provided as lambda envs
+PROJECT_NAME = os.getenv("project_name")
 REGION_NAME = os.getenv("region_name")
 SDLC_STAGE = os.getenv("sdlc_stage")
 ACCOUNT_ID = os.getenv("account_id")
@@ -13,6 +15,26 @@ CONFIG_S3_BUCKET = os.getenv("config_s3_bucket")
 EXTRACT_S3_BUCKET = os.getenv("extract_s3_bucket")
 EXTRACT_TRACKING_TABLE = os.getenv("extract_tracking_table")
 LOGGER = get_logger()
+
+
+def trigger_existing_sfn(source_name: str, etl_type: str):
+    client = boto3.client("stepfunctions")
+    next_sfn_name = f"{PROJECT_NAME}_{source_name}_{etl_type}"
+    next_sfn_arn = (
+        f"arn:aws:states:{REGION_NAME}:{ACCOUNT_ID}:stateMachine:{next_sfn_name}"
+    )
+
+    try:
+        return client.start_execution(
+            stateMachineArn=next_sfn_arn,
+            name=next_sfn_name,
+            input="string",
+        )
+    except ClientError as error:
+        if error.response["Error"]["Code"] == "EntityAlreadyExists":
+            LOGGER.info(f"{next_sfn_arn} does not exists so will not run next ETL step")
+        else:
+            raise error
 
 
 def push_sns_message(source_topic_arn: str, subject: str, message: str) -> dict:
@@ -57,10 +79,9 @@ def lambda_handler(event, context):
             if _job_run_type == "glue":
                 job_name = _event["JobName"]
                 arguments = _event["Arguments"]
+
                 source_topic_arn = arguments["--source_topic_arn"]
-                sns_subject = (
-                    f"Error:{error} occurred during Glue Job Run for :{job_name}'"
-                )
+                sns_subject = f"{SDLC_STAGE}: Error:{error} occurred during Glue Job Run for :{job_name}'"
                 sns_message = f"""
                                The error raised was:\n {error_message}
                                 """.strip()
@@ -71,13 +92,24 @@ def lambda_handler(event, context):
             if _job_run_type == "glue":
                 job_name = _event["JobName"]
                 arguments = _event["Arguments"]
+
+                source_name = arguments["--source_name"]
+                etl_type = arguments["--etl_type"]
+
                 source_topic_arn = arguments["--source_topic_arn"]
-                sns_subject = f"Run Succeeded for Glue Job: {job_name}"
+                sns_subject = f"{SDLC_STAGE}: Run Succeeded for Glue Job: {job_name}"
                 sns_message = f"""
                 Glue Job Run Succeeded for {arguments['--source_type']} source {arguments['--extract_table']}
                 """.strip()
             else:
                 raise ValueError("Cloud not define SNS payload from event payload")
+
+            # Once messages are sent, determine of a transform step function exists to run
+            # trying to make use of as little AWS services as possible to keep things simple
+            trigger_result = trigger_existing_sfn(
+                source_name=source_name, etl_type=etl_type
+            )
+            LOGGER.info(f"Trigger Existing SFN: {trigger_result}")
 
         LOGGER.info(f"sns_subject: {sns_subject}\nsns_message: {sns_message}")
         push_sns_message(
